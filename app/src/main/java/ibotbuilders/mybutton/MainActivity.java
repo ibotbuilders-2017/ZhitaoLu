89afa458b82991b3395790be108c6267fafb2cf0
Yellowstoneibot <zhitaolu2000@gmail.com>
2017-01-31 21:51:52 -0500

    add image button

--- app/src/main/java/ibotbuilders/mybutton/MainActivity.java
+++ app/src/main/java/ibotbuilders/mybutton/MainActivity.java
@@ -10,10 +10,15 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     Button btnCTime;
     EditText txtCTime;
+    Button SchoolButton, HomeworkButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View vw) {
                 txtCTime.setText(new Date().toString());
+            }
+        });
+
+        SchoolButton = (Button) findViewById(R.id.GoSchool);//get id of button 1
+        HomeworkButton = (Button) findViewById(R.id.DoHomework);//get id of button 2
+
+        SchoolButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "TimeToSchool", Toast.LENGTH_LONG).show();//display the text of button1
+            }
+        });
+        HomeworkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "HomeworkTime", Toast.LENGTH_LONG).show();//display the text of button2
             }
         });
     }
